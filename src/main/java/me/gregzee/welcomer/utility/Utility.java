32ba1fe394e587d389fcf78daf441cf3a64afe31
Gregzee <95419312+Gregzeee@users.noreply.github.com>
2024-07-20 19:30:19 +0300

    Added a playsound method

--- src/main/java/me/gregzee/welcomer/utility/Utility.java
+++ src/main/java/me/gregzee/welcomer/utility/Utility.java
@@ -1,10 +1,18 @@
 package me.gregzee.welcomer.utility;
 
 import net.md_5.bungee.api.ChatColor;
+import org.bukkit.Sound;
+import org.bukkit.entity.Player;
 
 public final class Utility {
 
 	public String colorize(String message) {
 		return ChatColor.translateAlternateColorCodes('&', message);
 	}
+
+	public void playSound(Player player, Sound sound, float volume, float pitch) {
+		player.playSound(player.getLocation(), sound, volume, pitch);
+	}
+
+	
 }
