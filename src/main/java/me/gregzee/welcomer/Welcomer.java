0e1eeefae314006a1ad499583a8545246431985f
Gregzee <95419312+Gregzeee@users.noreply.github.com>
2024-07-20 14:25:30 +0300

    Added a method for both oneable and ondisable

--- src/main/java/me/gregzee/welcomer/Welcomer.java
+++ src/main/java/me/gregzee/welcomer/Welcomer.java
@@ -2,21 +2,45 @@ package me.gregzee.welcomer;
 
 import lombok.Getter;
 import me.gregzee.welcomer.manager.StartupManager;
-import org.bstats.bukkit.Metrics;
+import me.gregzee.welcomer.utility.Utility;
+import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
 @Getter
 public class Welcomer extends JavaPlugin {
+
+    @Getter
+    private final Utility utility = new Utility();
 
     @Override
     public void onEnable() {
 
         new StartupManager(this);
 
+        sendEnableMessage();
+
     }
 
     @Override
     public void onDisable() {
-        getLogger().info("Plugin has been uninitialized!");
+        sendDisableMessage();
+    }
+
+    private void sendEnableMessage() {
+        ConsoleCommandSender console = getServer().getConsoleSender();
+        console.sendMessage(utility.colorize("&7-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="));
+        console.sendMessage(utility.colorize("                 &eWelcomer"));
+        console.sendMessage(utility.colorize("          Plugin has been enabled"));
+        console.sendMessage(utility.colorize("&7-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="));
+        console.sendMessage(utility.colorize("               &cMade by Gregzee"));
+    }
+
+    private void sendDisableMessage() {
+        ConsoleCommandSender console = getServer().getConsoleSender();
+        console.sendMessage(utility.colorize("&7-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="));
+        console.sendMessage(utility.colorize("                 &eWelcomer"));
+        console.sendMessage(utility.colorize("          Plugin has been disabled"));
+        console.sendMessage(utility.colorize("&7-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="));
+        console.sendMessage(utility.colorize("               &cMade by Gregzee"));
     }
 }
