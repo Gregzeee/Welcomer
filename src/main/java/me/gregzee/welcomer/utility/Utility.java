202ed4723d46838cc31b5e1c63d8588f1182f391
Gregzee <95419312+Gregzeee@users.noreply.github.com>
2024-07-20 14:24:57 +0300

    Added a colorize method

--- src/main/java/me/gregzee/welcomer/utility/Utility.java
+++ src/main/java/me/gregzee/welcomer/utility/Utility.java
@@ -1,4 +1,10 @@
 package me.gregzee.welcomer.utility;
 
+import net.md_5.bungee.api.ChatColor;
+
 public class Utility {
+
+	public String colorize(String message) {
+		return ChatColor.translateAlternateColorCodes('&', message);
+	}
 }
