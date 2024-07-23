8864cc96b06045b0bc030669cf85af7f546336e9
Gregzee <95419312+Gregzeee@users.noreply.github.com>
2024-07-23 16:02:21 +0300

    Added a new method for parsing placeholders Improved the docstrings

--- src/main/java/me/gregzee/welcomer/utility/Utility.java
+++ src/main/java/me/gregzee/welcomer/utility/Utility.java
@@ -1,5 +1,6 @@
 package me.gregzee.welcomer.utility;
 
+import me.clip.placeholderapi.PlaceholderAPI;
 import net.md_5.bungee.api.ChatColor;
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
@@ -8,8 +9,8 @@ public final class Utility {
 
 	/**
 	 * Colorizes a string
-	 * @param message
-	 * @return
+	 * @param message The message to colorize
+	 * @return the colorized message
 	 */
 	public String colorize(String message) {
 		return ChatColor.translateAlternateColorCodes('&', message);
@@ -17,14 +18,22 @@ public final class Utility {
 
 	/**
 	 * Plays a sound to a player
-	 * @param player
-	 * @param sound
-	 * @param volume
-	 * @param pitch
+	 * @param player The player to play the sound to
+	 * @param sound The sound to play
+	 * @param volume The volume of the sound
+	 * @param pitch The pitch of the sound
 	 */
 	public void playSound(Player player, Sound sound, float volume, float pitch) {
 		player.playSound(player.getLocation(), sound, volume, pitch);
 	}
 
-	
+	/**
+	 * Replaces placeholders with their actual values in a string
+	 * @param player The player to replace placeholders for
+	 * @param message The message to replace placeholders in
+	 * @return the message with placeholders replaced
+	 */
+	public String parsePlaceholders(Player player, String message) {
+		return PlaceholderAPI.setPlaceholders(player, message);
+	}
 }
