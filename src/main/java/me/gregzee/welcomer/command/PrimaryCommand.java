0d7040c85c22b0499f012b82c4f780ea8952b27e
Gregzee <95419312+Gregzeee@users.noreply.github.com>
2024-07-23 16:01:57 +0300

    Add a tabcompleter

--- src/main/java/me/gregzee/welcomer/command/PrimaryCommand.java
+++ src/main/java/me/gregzee/welcomer/command/PrimaryCommand.java
@@ -3,15 +3,42 @@ package me.gregzee.welcomer.command;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+import org.bukkit.entity.Player;
+import org.bukkit.util.StringUtil;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 
-public final class PrimaryCommand implements CommandExecutor {
+public final class PrimaryCommand implements CommandExecutor, TabCompleter {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 
-
-
         return true;
+    }
+
+    private static final String[] COMMANDS = {"menu", "reload"};
+
+    @Override
+    public @NotNull List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String s, @NotNull String[] args) {
+        if (!(sender instanceof Player)) {
+            return new ArrayList<>();
+        }
+
+        List<String> completions = new ArrayList<>();
+        List<String> suggestions = new ArrayList<>();
+
+        if (args.length == 1) {
+            completions.addAll(Arrays.asList(COMMANDS));
+        }
+
+        StringUtil.copyPartialMatches(args[0], completions, suggestions);
+        suggestions.sort(String.CASE_INSENSITIVE_ORDER);
+
+        return suggestions;
     }
 }
