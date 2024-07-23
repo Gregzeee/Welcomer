f644d4f81c35a4a4246cb6574a5a03b5be188ba3
Gregzee <95419312+Gregzeee@users.noreply.github.com>
2024-07-23 16:31:25 +0300

    Finished QuitListener

--- src/main/java/me/gregzee/welcomer/listener/QuitListener.java
+++ src/main/java/me/gregzee/welcomer/listener/QuitListener.java
@@ -1,11 +1,31 @@
 package me.gregzee.welcomer.listener;
 
+import me.gregzee.welcomer.Welcomer;
+import me.gregzee.welcomer.manager.ConfigManager;
+import me.gregzee.welcomer.utility.Utility;
+import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerQuitEvent;
 
 public final class QuitListener implements Listener {
 
+    private final Welcomer instance = Welcomer.getInstance();
+
+    private final ConfigManager configManager = Welcomer.getConfigManager();
+
+    private final Utility utility  = Welcomer.getUtility();
+
     public void onPlayerQuit(PlayerQuitEvent event) {
+
+        Player player = event.getPlayer();
+
+        String quitMessage = utility.parsePlaceholders(player, ConfigManager.QuitWelcome.getQuitMessage());
+
+        if (!ConfigManager.QuitWelcome.isEnabled()) {
+            event.setQuitMessage(null);
+        } else {
+            event.setQuitMessage(utility.colorize(quitMessage));
+        }
 
     }
 }
