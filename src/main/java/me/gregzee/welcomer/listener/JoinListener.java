430bfa9d0fe9ba7aac6e4e76ba9f0656ff91af4f
Gregzee <95419312+Gregzeee@users.noreply.github.com>
2024-07-28 21:37:14 +0300

    Added JoinWelcome, MOTD, actionbar, title

--- src/main/java/me/gregzee/welcomer/listener/JoinListener.java
+++ src/main/java/me/gregzee/welcomer/listener/JoinListener.java
@@ -1,14 +1,58 @@
 package me.gregzee.welcomer.listener;
 
+import me.clip.placeholderapi.libs.kyori.adventure.platform.facet.Facet;
+import me.gregzee.welcomer.Welcomer;
+import me.gregzee.welcomer.manager.ConfigManager;
+import me.gregzee.welcomer.utility.Utility;
+import net.md_5.bungee.api.ChatMessageType;
+import net.md_5.bungee.api.chat.TextComponent;
+import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+
+import java.awt.*;
 
 /**
  * Listener for player join events
  */
 public final class JoinListener implements Listener {
 
-    public void onPlayerJoin(PlayerJoinEvent event) {
+    private final Utility utility = Welcomer.getUtility();
 
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        final Player player = event.getPlayer();
+
+        /* MOTD */
+        if (ConfigManager.MOTD.isEnabled()) {
+            utility.loopMOTD(player);
+        }
+
+        /* Join Message */
+        if (ConfigManager.JoinWelcome.isEnabled()) {
+            final String joinMessage = ConfigManager.JoinWelcome.getJoinMessage();
+
+            if (player.hasPlayedBefore()) {
+                event.setJoinMessage(utility.colorize(utility.parsePlaceholders(player, joinMessage)));
+            } else {
+                event.setJoinMessage(utility.colorize(utility.parsePlaceholders(player, ConfigManager.JoinWelcome.getFirstJoinMessage())));
+            }
+        }
+
+        /* Title */
+        if (ConfigManager.TitleWelcome.isEnabled()) {
+            final String title = utility.colorize(utility.parsePlaceholders(player, ConfigManager.TitleWelcome.getTitle()));
+            final String subtitle = utility.colorize(utility.parsePlaceholders(player, ConfigManager.TitleWelcome.getSubtitle()));
+
+            utility.sendTitle(player, title, subtitle);
+        }
+
+        /* ActionBar */
+        if (ConfigManager.ActionBarWelcome.isEnabled()) {
+            final String actionBarMessage = utility.colorize(utility.parsePlaceholders(player, ConfigManager.ActionBarWelcome.getMessage()));
+
+            player.spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacyText(actionBarMessage));
+            // TODO - Move to adventure
+            // TODO - Move sound to its own place and then use it here
+        }
     }
 }
