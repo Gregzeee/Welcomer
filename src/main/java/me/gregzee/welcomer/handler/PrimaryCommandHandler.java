c90739f7af0106ae7f6e5ab7b4bf72bea3ff92f7
Gregzee <95419312+Gregzeee@users.noreply.github.com>
2024-07-29 16:59:25 +0300

    Finished handleReloadCommand

--- src/main/java/me/gregzee/welcomer/handler/PrimaryCommandHandler.java
+++ src/main/java/me/gregzee/welcomer/handler/PrimaryCommandHandler.java
@@ -1,12 +1,38 @@
 package me.gregzee.welcomer.handler;
 
+import me.gregzee.welcomer.Welcomer;
+import me.gregzee.welcomer.manager.ConfigManager;
+import me.gregzee.welcomer.utility.Utility;
+import org.bukkit.command.CommandSender;
+
 public final class PrimaryCommandHandler {
 
-	public static void handleMenuCommand() {
+	private static final ConfigManager configManager = Welcomer.getConfigManager();
+	private static final Utility utility = Welcomer.getUtility();
+
+	/**
+	 * Handles the menu command
+	 * @param sender The player who executed the command
+	 */
+	public static boolean handleMenuCommand(final CommandSender sender) {
 		// handle menu command
+		return true;
 	}
 
-	public static void handleReloadCommand() {
-		// handle reload command
+	/**
+	 * Handles the reload command
+	 * @param sender The player who executed the command
+	 */
+	public static boolean handleReloadCommand(final CommandSender sender) {
+		if (!sender.hasPermission("welcomer.commands.reload")) {
+			sender.sendMessage(utility.colorize(ConfigManager.getPrefix() + " " + ConfigManager.getNoPermissionMessage()));
+			return true;
+		}
+
+		long now = System.currentTimeMillis();
+		configManager.load();
+		long result = System.currentTimeMillis() - now;
+		sender.sendMessage(utility.colorize(ConfigManager.getPrefix() + " &aConfiguration reloaded in &f" + result + "ms"));
+		return true;
 	}
 }
