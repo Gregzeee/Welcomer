7812228ada571f204cddf62174413bd1c505e236
Gregzee <95419312+Gregzeee@users.noreply.github.com>
2024-07-29 16:23:03 +0300

    Create new PrimaryCommandHandler for PrimaryCommand

---
+++ src/main/java/me/gregzee/welcomer/handler/PrimaryCommandHandler.java
@@ -0,0 +1,12 @@
+package me.gregzee.welcomer.handler;
+
+public final class PrimaryCommandHandler {
+
+	public static void handleMenuCommand() {
+		// handle menu command
+	}
+
+	public static void handleReloadCommand() {
+		// handle reload command
+	}
+}
