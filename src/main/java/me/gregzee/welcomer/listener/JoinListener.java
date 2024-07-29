b77ace1bbc32aeb941cf7c3871885bc93e281f58
Gregzee <95419312+Gregzeee@users.noreply.github.com>
2024-07-29 16:13:07 +0300

    Removed the add more comments todo
    Added an if statement for the new sound section

--- src/main/java/me/gregzee/welcomer/listener/JoinListener.java
+++ src/main/java/me/gregzee/welcomer/listener/JoinListener.java
@@ -1,15 +1,16 @@
 package me.gregzee.welcomer.listener;
 
+import me.clip.placeholderapi.libs.kyori.adventure.Adventure;
 import me.gregzee.welcomer.Welcomer;
 import me.gregzee.welcomer.manager.ConfigManager;
 import me.gregzee.welcomer.utility.Utility;
 import net.md_5.bungee.api.ChatMessageType;
 import net.md_5.bungee.api.chat.TextComponent;
+import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
-// TODO - Add more comments
 /**
  * Listener for player join events
  */
@@ -26,31 +27,35 @@ public final class JoinListener implements Listener {
         }
 
         /* Join Message */
-        if (ConfigManager.JoinWelcome.isEnabled()) {
-            final String joinMessage = ConfigManager.JoinWelcome.getJoinMessage();
+        if (ConfigManager.Join.isEnabled()) {
+            final String joinMessage = ConfigManager.Join.getJoinMessage();
 
             if (player.hasPlayedBefore()) {
                 event.setJoinMessage(utility.colorize(utility.parsePlaceholders(player, joinMessage)));
             } else {
-                event.setJoinMessage(utility.colorize(utility.parsePlaceholders(player, ConfigManager.JoinWelcome.getFirstJoinMessage())));
+                event.setJoinMessage(utility.colorize(utility.parsePlaceholders(player, ConfigManager.Join.getFirstJoinMessage())));
             }
         }
 
         /* Title */
-        if (ConfigManager.TitleWelcome.isEnabled()) {
-            final String title = utility.colorize(utility.parsePlaceholders(player, ConfigManager.TitleWelcome.getTitle()));
-            final String subtitle = utility.colorize(utility.parsePlaceholders(player, ConfigManager.TitleWelcome.getSubtitle()));
+        if (ConfigManager.Title.isEnabled()) {
+            final String title = utility.colorize(utility.parsePlaceholders(player, ConfigManager.Title.getTitle()));
+            final String subtitle = utility.colorize(utility.parsePlaceholders(player, ConfigManager.Title.getSubtitle()));
 
             utility.sendTitle(player, title, subtitle);
         }
 
         /* ActionBar */
-        if (ConfigManager.ActionBarWelcome.isEnabled()) {
-            final String actionBarMessage = utility.colorize(utility.parsePlaceholders(player, ConfigManager.ActionBarWelcome.getMessage()));
+        if (ConfigManager.ActionBar.isEnabled()) {
+            final String actionBarMessage = utility.colorize(utility.parsePlaceholders(player, ConfigManager.ActionBar.getMessage()));
 
-            player.spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacyText(actionBarMessage));
             // TODO - Move to adventure
-            // TODO - Move sound to its own place and then use it here
+            player.spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacyText(actionBarMessage));
+        }
+
+        /* Sound */
+        if (ConfigManager.Sound.isEnabled()) {
+            player.playSound(player.getLocation(), Sound.valueOf(ConfigManager.Sound.getSound()), ConfigManager.Sound.getVolume(), ConfigManager.Sound.getPitch());
         }
     }
 }
