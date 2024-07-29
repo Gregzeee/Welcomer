4a847b8e4758e6661f42764fafbd599f65c95450
Gregzee <95419312+Gregzeee@users.noreply.github.com>
2024-07-29 16:36:05 +0300

    Finished the logic

--- src/main/java/me/gregzee/welcomer/command/PrimaryCommand.java
+++ src/main/java/me/gregzee/welcomer/command/PrimaryCommand.java
@@ -1,5 +1,9 @@
 package me.gregzee.welcomer.command;
 
+import me.gregzee.welcomer.Welcomer;
+import me.gregzee.welcomer.handler.PrimaryCommandHandler;
+import me.gregzee.welcomer.manager.ConfigManager;
+import me.gregzee.welcomer.utility.Utility;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -17,8 +21,20 @@ import java.util.List;
  */
 public final class PrimaryCommand implements CommandExecutor, TabCompleter {
 
+    private final Utility utility = Welcomer.getUtility();
+
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+        if ("menu".equalsIgnoreCase(args[0])) {
+            PrimaryCommandHandler.handleMenuCommand();
+            return true;
+        } else if ("reload".equalsIgnoreCase(args[0])) {
+            PrimaryCommandHandler.handleReloadCommand();
+            return true;
+        } else {
+            sender.sendMessage(utility.colorize(ConfigManager.getPrefix() + " &cInvalid usage! &f/welcomer <menu/reload>"));
+        }
 
         return true;
     }
