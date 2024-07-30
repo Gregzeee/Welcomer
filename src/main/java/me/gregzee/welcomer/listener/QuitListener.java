6f5cd85b8056870981a00a61f7a26fa848350072
Gregzee <95419312+Gregzeee@users.noreply.github.com>
2024-07-30 15:17:02 +0300

    Renamed parsePlaceholders method to setPlaceholders
    Removed unused fields
    
    Simplified the if statement

--- src/main/java/me/gregzee/welcomer/listener/QuitListener.java
+++ src/main/java/me/gregzee/welcomer/listener/QuitListener.java
@@ -3,32 +3,30 @@ package me.gregzee.welcomer.listener;
 import me.gregzee.welcomer.Welcomer;
 import me.gregzee.welcomer.manager.ConfigManager;
 import me.gregzee.welcomer.utility.Utility;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerQuitEvent;
+
+import java.util.logging.Level;
 
 /**
  * Listener for player quit events
  */
 public final class QuitListener implements Listener {
 
-    private final Welcomer instance = Welcomer.getInstance();
-
-    private final ConfigManager configManager = Welcomer.getConfigManager();
-
     private final Utility utility  = Welcomer.getUtility();
 
+    @EventHandler
     public void onPlayerQuit(PlayerQuitEvent event) {
 
         final Player player = event.getPlayer();
 
-        final String quitMessage = utility.parsePlaceholders(player, ConfigManager.Quit.getQuitMessage());
+        if (ConfigManager.Quit.isEnabled()) {
+            final String quitMessage = utility.colorize(utility.setPlaceholders(player, ConfigManager.Quit.getQuitMessage()));
 
-        if (!ConfigManager.Quit.isEnabled()) {
-            event.setQuitMessage(null);
-        } else {
-            event.setQuitMessage(utility.colorize(quitMessage));
+            event.setQuitMessage(quitMessage);
         }
-
     }
 }
