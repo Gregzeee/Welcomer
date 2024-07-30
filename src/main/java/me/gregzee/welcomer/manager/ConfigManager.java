521b82d44fad4e121e9aa77792625867e226325c
Gregzee <95419312+Gregzeee@users.noreply.github.com>
2024-07-30 15:10:39 +0300

    Added a debug method for debugging values in the future.
    Added a null check at the beginning of load()
    
    Added more comments

--- src/main/java/me/gregzee/welcomer/manager/ConfigManager.java
+++ src/main/java/me/gregzee/welcomer/manager/ConfigManager.java
@@ -2,6 +2,9 @@ package me.gregzee.welcomer.manager;
 
 import lombok.Getter;
 import me.gregzee.welcomer.Welcomer;
+import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.FileConfiguration;
+
 import java.util.List;
 
 /**
@@ -115,6 +118,11 @@ public final class ConfigManager {
 	 * Load the configuration file
 	 */
 	public void load() {
+		FileConfiguration config = instance.getConfig();
+		if (config == null) {
+			instance.getLogger().severe("Config is null in ConfigManager.load()");
+			return;
+		}
 
 		// General
 		prefix = getString("Prefix");
@@ -153,25 +161,102 @@ public final class ConfigManager {
 
 		// GUI
 		GUI.enabled = getBoolean("GUI.Enabled");
+//
+//		// Debug output | Used for testing
+//		debug();
 	}
-	
+
+	/********************************************************************************************/
+	/*                 Short versions of instance.getConfig().get...(path);                     */
+
+	/**
+	 * Get a string from the configuration file
+	 * @param path The path to the string
+	 * @return The string
+	 */
 	private String getString(String path) {
 		return instance.getConfig().getString(path);
 	}
 
+	/**
+	 * Get a boolean from the configuration file
+	 * @param path The path to the boolean
+	 * @return The boolean
+	 */
 	private boolean getBoolean(String path) {
 		return instance.getConfig().getBoolean(path);
 	}
 
+	/**
+	 * Get a list of strings from the configuration file
+	 * @param path The path to the list
+	 * @return The list
+	 */
 	private List<String> getStringList(String path) {
 		return instance.getConfig().getStringList(path);
 	}
 
+	/**
+	 * Get an integer from the configuration file
+	 * @param path The path to the integer
+	 * @return The integer
+	 */
 	private int getInt(String path) {
 		return instance.getConfig().getInt(path);
 	}
 
+	/**
+	 * Get a double from the configuration file
+	 * @param path The path to the double
+	 * @return The double
+	 */
 	private double getDouble(String path) {
 		return instance.getConfig().getDouble(path);
+	}
+
+	/*                 Short versions of instance.getConfig().get...(path);                     */
+	/********************************************************************************************/
+
+	/**
+	 * Log the configuration values for debugging | Used for testing
+	 */
+	public void debug() {
+		// General
+		Bukkit.getLogger().info("Prefix: " + prefix);
+		Bukkit.getLogger().info("No Permission Message: " + noPermissionMessage);
+
+		// MOTD
+		Bukkit.getLogger().info("MOTD Enabled: " + MOTD.enabled);
+		Bukkit.getLogger().info("MOTD Messages: " + MOTD.messages);
+
+		// Join
+		Bukkit.getLogger().info("Join Enabled: " + Join.enabled);
+		Bukkit.getLogger().info("First Join Message: " + Join.firstJoinMessage);
+		Bukkit.getLogger().info("Join Message: " + Join.joinMessage);
+
+		// Quit
+		Bukkit.getLogger().info("Quit Enabled: " + Quit.enabled);
+		Bukkit.getLogger().info("Quit Message: " + Quit.quitMessage);
+
+		// Title
+		Bukkit.getLogger().info("Title Enabled: " + Title.enabled);
+		Bukkit.getLogger().info("Title: " + Title.title);
+		Bukkit.getLogger().info("Subtitle: " + Title.subtitle);
+		Bukkit.getLogger().info("FadeIn: " + Title.fadeIn);
+		Bukkit.getLogger().info("Stay: " + Title.stay);
+		Bukkit.getLogger().info("FadeOut: " + Title.fadeOut);
+
+		// ActionBar
+		Bukkit.getLogger().info("ActionBar Enabled: " + ActionBar.enabled);
+		Bukkit.getLogger().info("ActionBar Message: " + ActionBar.message);
+
+		// Sound
+		Bukkit.getLogger().info("Sound Enabled: " + Sound.enabled);
+		Bukkit.getLogger().info("Sound: " + Sound.sound);
+		Bukkit.getLogger().info("Volume: " + Sound.volume);
+		Bukkit.getLogger().info("Pitch: " + Sound.pitch);
+
+		// GUI
+		Bukkit.getLogger().info("GUI Enabled: " + GUI.enabled);
 	}
 }
